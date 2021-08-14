fc93b1077604fcb37e6fc95abaf57d9a78569c2a
TGeronimo <thi.matheus@gmail.com>
2021-08-13 23:13:05 -0300

    Final version
    > Teste de acesso à documentação bem sucedido após inserir implementation('io.springfox:springfox-boot-starter:3.0.0') no build.gradle
    > Acessar a documentação no browser: http://localhost:8081/swagger-ui/#/

--- src/main/java/com/diocontroledeponto/ControleDePontoEAcesso/controller/JornadaTrabalhoController.java
+++ src/main/java/com/diocontroledeponto/ControleDePontoEAcesso/controller/JornadaTrabalhoController.java
@@ -2,9 +2,11 @@ package com.diocontroledeponto.ControleDePontoEAcesso.controller;
 
 import com.diocontroledeponto.ControleDePontoEAcesso.model.JornadaTrabalho;
 import com.diocontroledeponto.ControleDePontoEAcesso.service.JornadaService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -17,26 +19,31 @@ public class JornadaTrabalhoController {
     JornadaService jornadaService;
 
     @PostMapping
+    @ApiOperation(value = "Cria uma jornada de trabalho")
     public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho) {
         return jornadaService.saveJornada(jornadaTrabalho);
     }
 
     @GetMapping
+    @ApiOperation(value = "Obtém lista de jornadas de trabalho")
     public List<JornadaTrabalho> getJornadaList() {
         return jornadaService.findAll();
     }
 
     @GetMapping("/{idJornada}")
+    @ApiOperation(value = "Obtém uma jornada de trabalho, de acordo com a ID informada")
     public ResponseEntity<JornadaTrabalho> getJornadaById(@PathVariable("idJornada") Long idJornada) throws NoSuchElementException {
         return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Jornada não encontrada!")));
     }
 
     @PutMapping
+    @ApiOperation(value = "Atualiza um jornada de trabalho")
     public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
         return jornadaService.updateJornada(jornadaTrabalho);
     }
 
     @DeleteMapping("/{idJornada}")
+    @ApiOperation(value = "Apaga a jornada de trabalho especificada pelo ID")
     public ResponseEntity<JornadaTrabalho> deleteById(@PathVariable("idJornada") Long idJornada) throws NoSuchElementException {
         try {
             jornadaService.deleteJornada(idJornada);
@@ -45,6 +52,5 @@ public class JornadaTrabalhoController {
         }
         return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
     }
-
 
 }
--- src/main/java/com/diocontroledeponto/ControleDePontoEAcesso/swagger/SwaggerConfig.java
+++ src/main/java/com/diocontroledeponto/ControleDePontoEAcesso/swagger/SwaggerConfig.java
@@ -4,13 +4,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.ModelSpecification;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,36 +20,45 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-@ConfigurationProperties("app.api")
-@ConditionalOnProperty(name="app.api.swagger.enable", havingValue = "true", matchIfMissing = false)
 public class SwaggerConfig {
 
-//	private String version;
-//	private String title;
-//	private String description;
-//	private String basePackage;
-//	private String contactName;
-//	private String contactEmail;
-
+	//ant("/**"))
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.diocontroledeponto.backend"))
+			.apis(RequestHandlerSelectors.basePackage("com.diocontroledeponto.ControleDePontoEAcesso"))
 			.paths(PathSelectors.any())
 			.build()
 			.apiInfo(apiInfo())
-                .globalOperationParameters(
-                        Collections.singletonList(
-                                new ParameterBuilder()
-                                        .name("Authorization")
-                                        .description("Header para Token JWT")
-                                        .modelRef(new ModelRef("string"))
-                                        .parameterType("header")
-                                        .required(false)
-                                        .build()
-                        )
-                );
+						.globalRequestParameters(
+						Collections.singletonList(
+								new RequestParameterBuilder()
+								.name("Authorization")
+								.in(ParameterType.HEADER)
+								.parameterIndex(1)
+								.description("Header para Token JWT")
+								.required(false)
+								.build()
+				)
+		);
+
+//		This was used by the instructor but is deprecated, so I use the
+//		new methods globalRequestParameters and RequestParameterBuilder as above.
+
+//                .globalOperationParameters(
+//                        Collections.singletonList(
+//                                new ParameterBuilder()
+//                                        .name("Authorization")
+//                                        .description("Header para Token JWT")
+//                                        .modelRef(new ModelRef("string"))
+//                                        .parameterType("header")
+//                                        .required(false)
+//                                        .build()
+//                        )
+//                );
+
+
 	}
 
 	@Bean
@@ -59,7 +69,7 @@ public class SwaggerConfig {
 			.version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-			.contact(new Contact("DIO", "https://web.digitalinnovation.one", "contato@digitalinnovationone.com.br"))
+			.contact(new Contact("Thiago Geronimo", "https://www.linkedin.com/in/thiagogeronimo/", "thi.matheus@gmail.com"))
 			.build();
 	}
 
